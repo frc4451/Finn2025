098dbfa6bda9688b140c486fd8a6bde69dd2a77d
parsevel <joseph.miles1006@gmail.com>
2025-01-11 16:46:11 -0500

    Added AdvantageKit Support SparkMax, do SIM next

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -1,13 +1,24 @@
 package frc.robot.subsystems.drive;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.util.Units;
+
 public class DriveConstants {
     public static final int kFrontLeftId = 1;
     public static final int kFrontRightId = 2;
     public static final int kBackLeftId = 3;
     public static final int kBackRightId = 4;
+    public static final int kPidgeonId = 5;
 
     public static final double kRampRateSeconds = 0.3;
 
     public static final boolean kLeftInverted = true;
     public static final boolean kRightInverted = false;
+
+    public static final MotorType kMotorType = MotorType.kBrushed;
+
+    public static final double kTrackWidthMeters = Units.inchesToMeters(26.0);
+    public static final double kWheelRadiusMeters = Units.inchesToMeters(6.0);
+    public static final double kMotorReduction = 1.0;
 }
--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems.drive;
 
 import java.util.function.DoubleSupplier;
 
+import org.littletonrobotics.junction.Logger;
+
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -11,86 +13,57 @@ import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive.WheelSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.controllers.ControllerConstants;
+import frc.robot.subsystems.drive.DriveIO.DriveIOInputs;
 import frc.robot.util.SparkUtil;
 
 public class DriveSubsystem implements Subsystem {
 
-    public final SparkMax frontLeft = new SparkMax(DriveConstants.kFrontLeftId, MotorType.kBrushed);
-    public final SparkMax frontRight = new SparkMax(DriveConstants.kFrontRightId, MotorType.kBrushed);
-    public final SparkMax backLeft = new SparkMax(DriveConstants.kBackLeftId, MotorType.kBrushed);
-    public final SparkMax backRight = new SparkMax(DriveConstants.kBackRightId, MotorType.kBrushed);
+    private final DriveIO io = new DriveIOSpark();
+    private final DriveIOInputsAutoLogged inputs = new DriveIOInputsAutoLogged();
+    private final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(DriveConstants.kTrackWidthMeters);
 
-    private final DifferentialDrive drive = new DifferentialDrive(frontLeft, frontRight);
 
     public DriveSubsystem() {
-        configureMotorSettings();
+
     }
 
     @Override
     public void periodic() {
-        // applied voltage
-        // current (amperage)
-        // temperature (celsius)
-        logMotor("FrontLeft", frontLeft);
-        logMotor("FrontRight", frontRight);
-        logMotor("BackLeft", backLeft);
-        logMotor("BackRight", backRight);
+        io.updateInputs(inputs);
+        Logger.processInputs("Drive", inputs);
+        if (DriverStation.isDisabled()) {
+            io.stop();
+        }
     }
 
-    private void logMotor(String motorName, SparkBase spark) {
-        SmartDashboard.putNumber(motorName + "/AppliedVoltage", spark.getBusVoltage() * spark.getAppliedOutput());
-        SmartDashboard.putNumber(motorName + "/Current", spark.getOutputCurrent());
-        SmartDashboard.putNumber(motorName + "/MotorTemperatureCelsius", spark.getMotorTemperature());
-
-    }
-
-    private void configureMotorSettings() {
-        SparkBaseConfig config = new SparkMaxConfig();
-        config.openLoopRampRate(DriveConstants.kRampRateSeconds)
-                .idleMode(IdleMode.kBrake);
-
-        config.inverted(DriveConstants.kLeftInverted);
-        SparkUtil.tryUntilOk(
-                frontLeft,
-                5,
-                () -> frontLeft.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
-
-        config.inverted(DriveConstants.kRightInverted);
-        SparkUtil.tryUntilOk(
-                frontRight,
-                5,
-                () -> frontRight.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
-
-        config.inverted(DriveConstants.kLeftInverted)
-                .follow(frontLeft);
-        SparkUtil.tryUntilOk(
-                backLeft,
-                5,
-                () -> backLeft.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
-
-        config.inverted(DriveConstants.kRightInverted)
-                .follow(frontRight);
-        SparkUtil.tryUntilOk(
-                backRight,
-                5,
-                () -> backRight.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
-
+    private void runClosedLoop(double leftMetersPerSec, double rightMetersPerSec) {
+        double leftRadPerSec = leftMetersPerSec / DriveConstants.kWheelRadiusMeters;
+        double rightRadPerSec = rightMetersPerSec / DriveConstants.kWheelRadiusMeters;
+        Logger.recordOutput("Drive/LeftRadPerSec", leftRadPerSec);
+        Logger.recordOutput("Drive/RightRadPerSec", rightRadPerSec);
+        io.setVelocity(leftRadPerSec, rightRadPerSec);
+        
     }
 
     public Command driveCommand(DoubleSupplier forward, DoubleSupplier rotation) {
         return Commands.run(() -> {
+            WheelSpeeds speeds;
             if (forward.getAsDouble() > ControllerConstants.kJoystickDeadband
                     && forward.getAsDouble() < -ControllerConstants.kJoystickDeadband) {
-                drive.curvatureDrive(forward.getAsDouble(), rotation.getAsDouble(), false);
+                speeds = DifferentialDrive.curvatureDriveIK(forward.getAsDouble(), rotation.getAsDouble(), false);
             } else {
-                drive.curvatureDrive(forward.getAsDouble(), rotation.getAsDouble() / 2.0, true);
+                speeds = DifferentialDrive.curvatureDriveIK(forward.getAsDouble(), rotation.getAsDouble(), true);
             }
+            runClosedLoop(speeds.left, speeds.right);
         }, this);
     }
 }
